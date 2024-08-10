78464683562595f3865a69329f5117519780a1a5
faqihh21 <fqhahmed@gmail.com>
2024-08-11 02:54:40 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+// templatemo 467 easy profile
+
+// PRELOADER
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+	 		 Image
+	 			], 	{duration: 3200, fade: 1300});
+		});
+})
